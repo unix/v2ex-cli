a71e653d5e4bb3fb108c3cb557c792f111e2a31f
WittBulter <nanazuimeng123@gmail.com>
2017-10-15 23:45:57 +0800

    Bin: opt cache

--- bin/v2-show.js
+++ bin/v2-show.js
@@ -1,12 +1,15 @@
 const posts = require('../src/pages/posts')
 const Table = require('cli-table2')
+const { storage } = require('../src/utils')
 const table = new Table({
   head: ['id', 'title', 'desc', 're', 'member'],
   colWidths: [10, 40, 50, 5, 10]
 })
 
-posts.index().then(indexs => {
-  const rows = indexs.map(row => ([
+posts.index().then(posts => {
+  if (!posts || !posts.length) return console.log('No content')
+  storage.set('posts', posts)
+  const rows = posts.map(row => ([
     String(row.id),
     String(row.title),
     String((row.content_rendered || '').match(/[\u4e00-\u9fa5]+/g)),
@@ -14,8 +17,7 @@ posts.index().then(indexs => {
     String((row.member || {}).username),
   ]))
   table.push(...rows)
+  console.log(String(table))
 })
-.catch(e => {
-  console.log('err:' + String(e))
-})
+.catch(e => console.log('err:' + String(e)))
 
