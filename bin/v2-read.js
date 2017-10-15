6cc20e91d36e342bf3b57e1895a34aac4512fe21
WittBulter <nanazuimeng123@gmail.com>
2017-10-16 01:12:47 +0800

    Bin: improve param check

--- bin/v2-read.js
+++ bin/v2-read.js
@@ -1,21 +1,22 @@
 const posts = require('../src/pages/posts')
-const Table = require('cli-table2')
 const commander = require('commander')
 const chalk = require('chalk')
-const { parse, storage } = require('../src/utils')
+const { storage } = require('../src/utils')
 
 // parse id
-parse()
+commander.parse(process.argv)
 
-const findPost = async(id) => {
+const show = p => {
+  console.log(`post: ${p.id}`)
+  console.log(chalk.black.bgWhite.bold(` -${p.title}- \n`))
+  console.log(`${p.content} \n`)
+}
+const findPost = async(id, cache = null) => {
   try {
+    if (cache) return show(cache)
     const post = await posts.show(id)
     if (!post || !post.length) return console.log('No content')
-    const p = post[0]
-    console.log(`post: ${id}`)
-    console.log(chalk.black.bgWhite.bold(` -${p.title}- `))
-    console.log('-------\n')
-    console.log(`${p.content} \n`)
+    show(post[0])
   } catch (e) {
     console.log('err:' + String(e))
   }
@@ -24,13 +25,17 @@ const findPost = async(id) => {
 // check id
 (async() => {
   const id = commander.args[0]
+  if (!id) {
+    console.log('id is required, like:', chalk.blue('v2 read 10'));
+    return process.exit(1)
+  }
   const postsStorage = await storage.get('posts')
   if (!postsStorage) return await findPost(id)
   
   let post = postsStorage.find(post => post.id === id)
-  if (post && post.id) return await findPost(post.id)
+  if (post && post.id) return await findPost(post.id, post)
   
   post = postsStorage.find(post => String(post.id).endsWith(id))
-  if (post && post.id) return await findPost(post.id)
+  if (post && post.id) return await findPost(post.id, post)
   await findPost(id)
 })()
--- bin/v2-show.js
+++ bin/v2-show.js
@@ -1,23 +1,37 @@
-const posts = require('../src/pages/posts')
 const Table = require('cli-table2')
+const commander = require('commander')
 const { storage } = require('../src/utils')
+const { index } = require('../src/pages/posts')
+
 const table = new Table({
   head: ['id', 'title', 'desc', 're', 'member'],
   colWidths: [10, 40, 50, 5, 10]
 })
 
-posts.index().then(posts => {
-  if (!posts || !posts.length) return console.log('No content')
-  storage.set('posts', posts).then()
-  const rows = posts.map(row => ([
-    String(row.id),
-    String(row.title),
-    String((row.content_rendered || '').match(/[\u4e00-\u9fa5]+/g)),
-    String(row.replies),
-    String((row.member || {}).username),
-  ]))
-  table.push(...rows)
-  console.log(String(table))
-})
-.catch(e => console.log('err:' + String(e)))
+// parse page
+commander.parse(process.argv)
 
+const findPostIndex = async(page) => {
+  try {
+    const posts = await index(page)
+    if (!posts || !posts.length) return console.log('No content')
+    storage.set('posts', posts).then()
+    const rows = posts.map(row => ([
+      String(row.id),
+      String(row.title),
+      String((row.content_rendered || '').match(/[\u4e00-\u9fa5]+/g)),
+      String(row.replies),
+      String((row.member || {}).username),
+    ]))
+    table.push(...rows)
+    console.log(String(table))
+  } catch (e) {
+    console.log('err:' + String(e))
+  }
+}
+
+// check id
+(async() => {
+  const page = commander.args[0]
+  await findPostIndex(page || 1)
+})()
