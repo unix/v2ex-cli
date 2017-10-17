34be6eb45715c06fe9acb6951abace2e9258d144
WittBulter <nanazuimeng123@gmail.com>
2017-10-17 23:26:54 +0800

    Pages: add page number support

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,7 +1,29 @@
+const cheerio = require('cheerio')
 const { request, apis, makeHeader } = require('../utils')
+const parse = async(page) => {
+  try {
+    const posts = await request({
+      uri: `${apis.recent}?p=${page}`,
+      method: 'GET',
+      headers: await makeHeader(),
+    })
+    const $ = cheerio.load(String(posts), {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    })
+  } catch (e) {
+    return e
+  }
+}
 
 module.exports = {
   index: async(page, nodeID) => {
+    if (page > 1) {
+      await parse(page)
+      return []
+    }
     try {
       const posts = await request({
         uri: nodeID ? `${apis.show}?node_id=${nodeID}` : apis.all,
