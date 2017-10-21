e67e6353d69e460d283fd313cf8cef9962b778e1
WittBulter <nanazuimeng123@gmail.com>
2017-10-21 21:06:12 +0800

    Utils: add write method

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -3,13 +3,13 @@ exists('./temp').then(bool => !bool && spawnSync('mkdir', ['./temp/']))
 
 module.exports = {
   storage: {
-    set: async (key, value) => {
+    set: async(key, value) => {
       const path = `./temp/${key}.txt`
       if (await exists(path)) spawnSync('rm', [path])
       await writeFile(path, JSON.stringify(value), 'utf-8')
     },
   
-    get: async (key) => {
+    get: async(key) => {
       const path = `./temp/${key}.txt`
       if (!await exists(path)) return null
       let result = {}
@@ -20,5 +20,18 @@ module.exports = {
       }
       return result
     },
+    
+    write: async(name, content) => {
+      const path = `./temp/${name}`
+      if (await exists(path)) spawnSync('rm', [path])
+      await writeFile(path, content, 'binary')
+      return path
+    },
+    
+    read: async(name) => {
+      const path = `./temp/${key}`
+      if (!await exists(path)) return null
+      return await readFile(path, 'utf-8')
+    }
   },
 }
