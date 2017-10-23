75632360b5a619c2bdf0f607a2328f483c0d4b10
WittBulter <nanazuimeng123@gmail.com>
2017-10-23 17:49:30 +0800

    Bin: use install replace login command

---
+++ bin/v2-install.js
@@ -0,0 +1,33 @@
+const commander = require('commander')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const ora = require('ora')
+const { storage } = require('../src/utils')
+const saveLog = new ora('save cookie..')
+
+// parse page
+commander.parse(process.argv)
+console.log(chalk.green.bold('Welcome, V2ER.'))
+// console.log(chalk.green('If you are interested, please give me a star: github.com/WittBulter/v2ex-cli'))
+
+new ora().start().info('Before use, you need to add some set items..')
+const promps = [{
+  type: 'input',
+  name: 'cookie',
+  message: 'cookie:',
+  validate: input => !!input
+}]
+
+
+// check id
+;(async() => {
+  const asnwers = await inquirer.prompt(promps)
+  const cookie = asnwers.cookie
+  if (cookie.length < 10 || !cookie.includes('A2=')) return console.log(chalk.red('bad cookie'))
+  
+  saveLog.start()
+  await storage.write('cookie', cookie)
+  saveLog.succeed('cookie saved')
+})()
+
+
