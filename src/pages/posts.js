40d2f1081996cef2770a561d4e2f58c8af6089ff
WittBulter <nanazuimeng123@gmail.com>
2017-10-23 20:55:54 +0800

    Pages: opt html parse

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,39 +1,40 @@
 const cheerio = require('cheerio')
 const { request, apis, makeHeader, storage } = require('../utils')
 
-const parse = async(page) => {
-  try {
-    const posts = await request({
-      uri: `${apis.recent}?p=${page}`,
-      method: 'GET',
-      headers: await makeHeader({
-        cookie: String(await storage.read('cookie.txt')),
-      }),
-    })
-    const $ = cheerio.load(String(posts))
-  } catch (e) {
-    console.log(e)
-    return e
-  }
-}
-
 module.exports = {
   index: async(page, nodeID) => {
-    if (page > 1) {
-      await parse(page)
-      return []
-    }
+    const uri = nodeID ? `${apis.go}/${nodeID}?p=${page}` : `${apis.recent}?p=${page}`
     try {
       const posts = await request({
-        uri: nodeID ? `${apis.show}?node_id=${nodeID}` : apis.all,
-        method: 'GET',
-        headers: await makeHeader(),
+        uri, method: 'GET',
+        headers: await makeHeader({
+          cookie: String(await storage.getCookie()),
+        }),
       })
-      return JSON.parse(posts)
+      if (String(posts).includes('<a href="/signin" class="top">登录</a>')) {
+        throw 'cookie not found, try run [v2 install] set it.'
+        return ''
+      }
+      const $ = cheerio.load(String(posts), { decodeEntities: true })
+      let result = []
+      $('span.item_title').each(function(index) {
+        const postLink = cheerio(cheerio(this).find('a')[0])
+        const userLink = cheerio(this).parents('tr').find('img.avatar').parent()
+        const links = cheerio(this).parents('tr').find('a.count_livid')
+        const replies = (links && links[0]) ? cheerio(links[0]).attr('href').split('reply')[1] : 0
+        result.push([
+          postLink.attr('href').split('#')[0].match(/\d+/g)[0],   // id
+          postLink.text(),    // title
+          replies,            // re
+          userLink.attr('href').split('member/')[1],    // member
+        ])
+      })
+      return result
     } catch (e) {
-      return e
+      throw e
     }
   },
+  
   show: async(id) => {
     try {
       const post = await request({
