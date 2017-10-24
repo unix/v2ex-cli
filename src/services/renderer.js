5e77caa7e38c71db8ae2b05ff656a88b8359fe41
WittBulter <nanazuimeng123@gmail.com>
2017-10-24 16:13:22 +0800

    Services: init renderer

---
+++ src/services/renderer.js
@@ -0,0 +1,36 @@
+const Table = require('cli-table2')
+const ora = require('ora')
+const { storage, histroy } = require('../utils')
+const { index } = require('../pages/posts')
+
+
+module.exports = {
+  renderPosts: async(page = 1, node = {}) => {
+    const fetchLog = new ora('fetching..').start()
+    const table = new Table({
+      head: ['id', 'title', 're', 'member'],
+      colWidths: [10, 60, 5, 15]
+    })
+  
+    try {
+      const posts = await index(page, node.name || null)
+      if (!posts || !posts.length) {
+        fetchLog.text = ''
+        return fetchLog.fail('no content')
+      }
+      storage.set('posts', posts)
+      table.push(...posts)
+      fetchLog.clear()
+      console.log(String(table))
+      
+      histroy.add('list', `${page}||${node.title || null}||${node.name || null}`)
+      return fetchLog.succeed((node && node.title ? `node: ${node.title}，` : `latest,`) + `page: ${page}`)
+    } catch (e) {
+      fetchLog.clear()
+      return fetchLog.fail('Err: ' + String(e))
+    }
+  },
+  
+  
+  
+}
