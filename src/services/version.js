0741665344b96dbd242a90ba55039a911cefec68
WittBulter <nanazuimeng123@gmail.com>
2017-10-24 19:02:46 +0800

    Services: update version tip

--- src/services/version.js
+++ src/services/version.js
@@ -1,16 +1,23 @@
 const chalk = require('chalk')
+const Table = require('cli-table2')
 const { request } = require('../utils')
 const pkg = require('../../package.json')
 
 module.exports = {
   check: async() => {
+    const table = new Table({ chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': '' }})
     const currentVersion = pkg.version
     const api = pkg.repository.update
     const githubPkg = JSON.parse(await request(api)) || {}
     const latestVersion = githubPkg.version
     if (latestVersion && latestVersion !== currentVersion) {
-      console.log(chalk.yellow(`V2EX CLI ${latestVersion} has been released.\n`))
-      console.log(chalk.yellow(`run [${chalk.green('npm update v2ex-cli -g')}] to update.\n`))
+      console.log('\n')
+      table.push(
+        [`   V2EX CLI ${latestVersion} has been released`],
+        [`run [${chalk.green('npm update v2ex-cli -g')}] to update`],
+      )
+      console.log(String(table))
+      console.log('\n')
     }
   },
   
